1e3d6fdcea52011b8139f58816a3a878fed11494
Elder Freitas <elderalvesf@gmail.com>
2026-05-13 12:36:49 -0400

    fix: wait for app auth and destination screen in navigation helpers

--- test/pageobjects/NavigationHelper.js
+++ test/pageobjects/NavigationHelper.js
@@ -1,25 +1,36 @@
 class NavigationHelper {
+  async waitForApp() {
+    await $('~home_screen').waitForDisplayed({ timeout: 30000 });
+  }
+
   async goToHome() {
+    await this.waitForApp();
     const el = await $('~nav_home');
-    await el.waitForDisplayed({ timeout: 15000 });
+    await el.waitForDisplayed({ timeout: 5000 });
     await el.click();
+    await $('~home_screen').waitForDisplayed({ timeout: 10000 });
   }
 
   async goToAgenda() {
+    await this.waitForApp();
     const el = await $('~nav_appointments');
-    await el.waitForDisplayed({ timeout: 15000 });
+    await el.waitForDisplayed({ timeout: 5000 });
     await el.click();
+    await $('~agenda_screen').waitForDisplayed({ timeout: 10000 });
   }
 
   async goToPatients() {
+    await this.waitForApp();
     const el = await $('~nav_patients');
-    await el.waitForDisplayed({ timeout: 15000 });
+    await el.waitForDisplayed({ timeout: 5000 });
     await el.click();
+    await $('~pacientes_screen').waitForDisplayed({ timeout: 10000 });
   }
 
   async goToFinanceiro() {
+    await this.waitForApp();
     const el = await $('~nav_financial');
-    await el.waitForDisplayed({ timeout: 15000 });
+    await el.waitForDisplayed({ timeout: 5000 });
     await el.click();
   }
 }
